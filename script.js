fdc199472df4d94b13acfe060d3466061cf71d0e
Shamit Mondal <shamitmondal880@gmail.com>
2026-01-02 13:50:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
+
+addBtn.addEventListener("click", addTask);
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") {
+        alert("Please enter a task!");
+        return;
+    }
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    
+    li.addEventListener("click", () => {
+        li.classList.toggle("completed");
+    });
+
+    
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+
+    deleteBtn.addEventListener("click", (e) => {
+        e.stopPropagation();
+        li.remove();
+    });
+
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+}
